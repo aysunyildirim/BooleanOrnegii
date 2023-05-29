726fbd42f91c1b85ad001524be1c881259bdd754
Aysun YILDIRIM <82328345+aysunyildirim@users.noreply.github.com>
2023-05-29 21:41:16 +0300

    Add project files.

---
+++ BooleanOrnegii/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+    Console.WriteLine("Kaç yaşındasın?");
+
+    int can = Convert.ToInt32(Console.ReadLine();
+
+    bool resit = can>=18; //18 e eşitse true,değilse false değerini döndürür. //> karsılaştırma anlamındadır.
+
+    Console.WriteLine();
+    Console.ReadLine();
